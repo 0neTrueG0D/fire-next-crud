fed964b652f11465e947692cf4db1db9e100f83b
zzedddd <melegend.forever@protonmail.com>
2023-10-18 20:36:35 +0530

    move header component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Header } from "@/app/components/server";
+
+const inter = Inter({ subsets: ["latin"], display: "block" });
 
 export const metadata: Metadata = {
     title: "Firebase Next.js CRUD",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Header />
+                {children}
+            </body>
         </html>
     );
 }
