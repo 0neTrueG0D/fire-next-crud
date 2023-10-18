e6cc1af015368c3ee0a12f9924766f7ca6a202e6
zzedddd <melegend.forever@protonmail.com>
2023-10-19 01:04:05 +0530

    fetch data dynamically on mount from firebase

--- src/app/components/client/UserDataTable.tsx
+++ src/app/components/client/UserDataTable.tsx
@@ -1,6 +1,37 @@
+import { useState, useEffect } from "react";
+import {
+    collection,
+    addDoc,
+    getDoc,
+    QuerySnapshot,
+    query,
+    onSnapshot,
+} from "firebase/firestore";
+
+import { db } from "@/app/firebase";
+
 export default function UserDataTable() {
+    const [data, setData] = useState([]);
+
+    // fetch data from firestore
+    useEffect(() => {
+        const q = query(collection(db, "users"));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const users: any = [];
+            querySnapshot.forEach((doc) => {
+                users.push({
+                    id: doc.id,
+                    ...doc.data(),
+                });
+            });
+            setData(users);
+            console.log("Current data: ", users);
+        });
+        return () => unsubscribe();
+    }, []);
+
     return (
-        <div className="overflow-x-auto border border-base-content border-opacity-20 rounded-box p-5 w-full">
+        <div className="overflow-auto border border-base-content border-opacity-20 rounded-box p-5 w-full max-h-[36rem]">
             <table className="table">
                 {/* head */}
                 <thead>
@@ -12,114 +43,37 @@ export default function UserDataTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    {/* row 1 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center space-x-3">
-                                <div>
-                                    <div className="font-bold">
-                                        Hart Hagerty
-                                    </div>
-                                    <div className="text-sm text-center opacity-50">
-                                        17, Male
-                                    </div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>email.issofuckingop@mail.com</td>
-                        <td>69 Pikachooo Street, Pune, Maharashtra India </td>
-                        <td>
-                            <div className="flex gap-5">
-                                <button className="btn btn-ghost btn-xs">
-                                    Edit
-                                </button>
-                                <button className="btn btn-ghost btn-xs">
-                                    Delete
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    {/* row 2 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center space-x-3">
-                                <div>
-                                    <div className="font-bold">
-                                        Hart Hagerty
-                                    </div>
-                                    <div className="text-sm opacity-50">
-                                        17, Male
+                    {data.map((user: any) => (
+                        <tr key={user.id}>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div>
+                                        <div className="font-bold">
+                                            {user.name}
+                                        </div>
+                                        <div className="text-sm text-center opacity-50">
+                                            {user.age}, {user.gender}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>email@mail.com</td>
-                        <td>Street, Pune, Maharashtra India </td>
-                        <td>
-                            <div className="flex gap-5">
-                                <button className="btn btn-ghost btn-xs">
-                                    Edit
-                                </button>
-                                <button className="btn btn-ghost btn-xs">
-                                    Delete
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    {/* row 3 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center space-x-3">
-                                <div>
-                                    <div className="font-bold">
-                                        Hart Hagerty
-                                    </div>
-                                    <div className="text-sm opacity-50">
-                                        17, Male
-                                    </div>
+                            </td>
+                            <td>{user.email}</td>
+                            <td>
+                                {user.street}, {user.city}, {user.state},{" "}
+                                {user.country}{" "}
+                            </td>
+                            <td>
+                                <div className="flex gap-5">
+                                    <button className="btn btn-ghost btn-xs">
+                                        Edit
+                                    </button>
+                                    <button className="btn btn-ghost btn-xs">
+                                        Delete
+                                    </button>
                                 </div>
-                            </div>
-                        </td>
-                        <td>email@mail.com</td>
-                        <td>Street, Pune, Maharashtra India </td>
-                        <td>
-                            <div className="flex gap-5">
-                                <button className="btn btn-ghost btn-xs">
-                                    Edit
-                                </button>
-                                <button className="btn btn-ghost btn-xs">
-                                    Delete
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    {/* row 4 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center space-x-3">
-                                <div>
-                                    <div className="font-bold">
-                                        Hart Hagerty
-                                    </div>
-                                    <div className="text-sm opacity-50">
-                                        17, Male
-                                    </div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>email@mail.com</td>
-                        <td>Street, Pune, Maharashtra India </td>
-                        <td>
-                            <div className="flex gap-5">
-                                <button className="btn btn-ghost btn-xs">
-                                    Edit
-                                </button>
-                                <button className="btn btn-ghost btn-xs">
-                                    Delete
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
